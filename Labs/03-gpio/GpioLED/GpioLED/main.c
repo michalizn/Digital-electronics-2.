a075878f413ed0ac1429c59e5e1afe2b66a30620
student <student@PC-625.urel.feec.vutbr.cz>
2021-10-13 13:25:02 +0200

    interrupts

---
+++ Labs/03-gpio/GpioLED/GpioLED/main.c
@@ -0,0 +1,18 @@
+/*
+ * GpioLED.c
+ *
+ * Created: 06.10.2021 15:36:55
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
